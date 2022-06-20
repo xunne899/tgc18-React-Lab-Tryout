cc472f59f5e5de0d5ee3176f4072e6c587ebf991
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-06-20 17:05:20 +0000

    added form handling

---
+++ form-handling/src/SurveyForm.js
@@ -0,0 +1,12 @@
+import React from "react" ;
+
+export default class SurveyForm extends React.Component {
+    state = {
+        'name':'',
+        'colour':'',
+        'country':''
+    }
+}
+
+
+
